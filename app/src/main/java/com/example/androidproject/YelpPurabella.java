b425f189419c44da667f4fd0ce0227b2793ea218
lyndaflower <umurerwalynda@gmail.com>
2019-10-18 21:00:46 +0200

    add folders

---
+++ app/src/main/java/com/example/androidproject/YelpPurabella.java
@@ -0,0 +1,64 @@
+
+package com.example.androidproject;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class YelpPurabella {
+
+    @SerializedName("businesses")
+    @Expose
+    private List<Business> businesses = null;
+    @SerializedName("total")
+    @Expose
+    private Integer total;
+    @SerializedName("region")
+    @Expose
+    private Region region;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public YelpPurabella() {
+    }
+
+    /**
+     * 
+     * @param region
+     * @param total
+     * @param businesses
+     */
+    public YelpPurabella(List<Business> businesses, Integer total, Region region) {
+        super();
+        this.businesses = businesses;
+        this.total = total;
+        this.region = region;
+    }
+
+    public List<Business> getBusinesses() {
+        return businesses;
+    }
+
+    public void setBusinesses(List<Business> businesses) {
+        this.businesses = businesses;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+}
